ad7c78b054939fbc46fd143ea51dc5651b83db8a
Agcpedro <agcpedro@yahoo.com.br>
2024-04-29 10:42:20 -0300

    Adicionar arquivos de projeto.

---
+++ RosenTrabADV2/Classes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosenTrabADV2
+{
+    public class Estudante
+    {
+        // Atributos
+        private string nome;
+        private int idade;
+        private double nota;
+        private string curso;
+        private int ConhecimentoGerais = 0;
+        public Dictionary<string, int> ConhecimentoMaterias = new Dictionary<string, int>();
+
+        // Propriedades
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Curso
+        {
+            get { return curso; }
+            set { curso = value; }
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public double Nota
+        {
+            get { return nota; }
+            set { nota = value; }
+        }
+
+        // Métodos
+        public void Estudar()
+        {
+            ConhecimentoGerais++; // Incrementa o conhecimento geral
+            Console.WriteLine("Conhecimento Gerais agora é " + ConhecimentoGerais);
+        }
+
+        // Sobrecarga do método Estudar
+        public void Estudar(string materia)
+        {
+            materia = materia.ToUpper(); // Converte a matéria para maiúsculas
+            if (!ConhecimentoMaterias.ContainsKey(materia))
+            {
+                ConhecimentoMaterias[materia] = 0;
+            }
+            ConhecimentoMaterias[materia]++;
+            Console.WriteLine("Conhecimento em " + materia + " agora é " + ConhecimentoMaterias[materia]);
+        }
+
+        public void FazerExame()
+        {
+            // Cálculo da nota para Conhecimento Geral
+            double notaConhecimentoGeral = 0 + ConhecimentoGerais;
+            Console.WriteLine("Nota Conhecimento Geral: " + notaConhecimentoGeral);
+
+            // Cálculo da nota para cada matéria
+            foreach (var materia in ConhecimentoMaterias.Keys)
+            {
+                double notaMateria = 0 + ConhecimentoGerais * 0.5 + ConhecimentoMaterias[materia];
+                Console.WriteLine("Nota " + materia + ": " + notaMateria);
+            }
+        }
+    }
+}
+
---
+++ RosenTrabADV2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using RosenTrabADV2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Estudante> estudantes = new List<Estudante>();
+        Estudante estudanteAtual = null;
+
+        while (true)
+        {
+            CriarLinha();
+            if (estudanteAtual != null)
+            {
+                Console.WriteLine("Aluno selecionado atual: " + estudanteAtual.Nome + ", Idade: " + estudanteAtual.Idade + ", Curso: " + estudanteAtual.Curso);
+            }
+            else
+            {
+                Console.WriteLine("Nenhum aluno selecionado.");
+            }
+            CriarLinha();
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1. Cadastrar novo aluno");
+            Console.WriteLine("2. Selecionar aluno");
+            Console.WriteLine("3. Estudar conhecimento geral");
+            Console.WriteLine("4. Estudar conhecimento específico");
+            Console.WriteLine("5. Fazer exame");
+            Console.WriteLine("6. Sair");
+            CriarLinha();
+
+            string input = Console.ReadLine();
+            int opcao;
+
+            if (!int.TryParse(input, out opcao))
+            {
+                Console.WriteLine("Por favor, insira um número válido.");
+                CriarLinha();
+                continue;
+            }
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Digite o nome do aluno:");
+                    string nome = Console.ReadLine();
+                    CriarLinha();
+                    Console.WriteLine("Digite a idade do aluno:");
+                    int idade = Convert.ToInt32(Console.ReadLine());
+                    CriarLinha();
+                    Console.WriteLine("Digite o curso do aluno:");
+                    string curso = Console.ReadLine();
+                    CriarLinha();
+                    Estudante novoEstudante = new Estudante { Nome = nome, Idade = idade, Curso = curso };
+                    estudantes.Add(novoEstudante);
+                    estudanteAtual = novoEstudante;
+                    Console.WriteLine("Aluno cadastrado com sucesso!");
+                    break;
+                case 2:
+                    Console.WriteLine("Alunos cadastrados:");
+                    foreach (var estudante in estudantes)
+                    {
+                        Console.WriteLine(estudante.Nome);
+                    }
+                    CriarLinha();
+                    Console.WriteLine("Digite o nome do aluno que deseja selecionar:");
+                    string nomeAluno = Console.ReadLine();
+                    CriarLinha();
+                    estudanteAtual = estudantes.Find(e => e.Nome == nomeAluno);
+                    if (estudanteAtual != null)
+                    {
+                        Console.WriteLine("Aluno selecionado com sucesso!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Aluno não encontrado!");
+                    }
+                    break;
+                case 3:
+                    if (estudanteAtual != null)
+                    {
+                        estudanteAtual.Estudar();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhum aluno selecionado!");
+                    }
+                    break;
+                case 4:
+                    if (estudanteAtual != null)
+                    {
+                        Console.WriteLine("Matérias já estudadas:");
+                        foreach (var materia in estudanteAtual.ConhecimentoMaterias.Keys)
+                        {
+                            Console.WriteLine(materia);
+                        }
+                        CriarLinha();
+                        Console.WriteLine("Digite a matéria que deseja estudar:");
+                        string materiaEstudo = Console.ReadLine();
+                        CriarLinha();
+                        estudanteAtual.Estudar(materiaEstudo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhum aluno selecionado!");
+                    }
+                    break;
+                case 5:
+                    if (estudanteAtual != null)
+                    {
+                        estudanteAtual.FazerExame();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhum aluno selecionado!");
+                    }
+                    break;
+                case 6:
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+
+    private static void CriarLinha()
+    {
+        Console.WriteLine("/-------------------------------------------------------------/");
+    }
+}
